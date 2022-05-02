44c12337c83be399a9d7a01074a3336aeb3b51e5
andlox73 <93976484+andlox73@users.noreply.github.com>
2022-05-03 01:22:38 +0200

    Update GestoreServer.java

--- src/esclientserverba/GestoreServer.java
+++ src/esclientserverba/GestoreServer.java
@@ -16,6 +16,8 @@ public class GestoreServer {
      */
     public static void main(String[] args) {
         Server srv = new Server(2000);
+        srv.inAscolto();
+        srv.scrivi("ciao client");
     }
     
 }
