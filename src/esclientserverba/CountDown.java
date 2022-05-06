513f629d3f45a1de349c528096e5532f5f43bd90
andlox73 <93976484+andlox73@users.noreply.github.com>
2022-05-06 12:39:47 +0200

    Update CountDown.java

--- src/esclientserverba/CountDown.java
+++ src/esclientserverba/CountDown.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package esclientserverba;
 
-/**
- *
- * @author Admin
- */
+import java.util.logging.level;
+import java.util.logging.logger;
+
 public class CountDown extends Thread{
+    private int CountDown;
+    
+    public CountDown(int CD){
+        this.CountDown = CD;
+    }
+   
+    @Override
+    public void run(){
+        for (int i = CountDown; i>0; i--){
+            try{
+                System.out.println(i);
+                sleep(1000);
+            }catch(InterruptedException ex){
+                Logger.getLogger.(CountDown.class.getName().log(level.SERVER, null, ex);
+            }
+        }
+    }
     
 }
